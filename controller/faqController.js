f6715052785c6ea5cb46e0d37369a418ec79a9af
Baljit Singh <thebaljitsinghin@gmail.com>
2025-02-01 01:24:31 +0530

    feat: added the search  by id and lang feature

--- controller/faqController.js
+++ controller/faqController.js
@@ -1,61 +1,82 @@
 import express from 'express';
 import Faq from "../model/faqModel.js"
+import client from "../config/redis.js";
 
-export const getAllFaq = async (req, res)=>{
 
 
-    console.log(req.query.lang);
-
+export const getAllFaq = async (req, res) => {
     try {
+        const lang = req.query.lang || 'en';
+        const cacheKey = `lan_${lang}`;
 
-        const lang = req.query.lang || "en";
+        const cachedFaqs = await client.get(cacheKey);
+        if (cachedFaqs) {
+            console.log('Cache hit');
+            return res.status(200).json({
+                success: true,
+                translatedFaqs: JSON.parse(cachedFaqs),
+            });
+        } else {
+            console.log('Not found in cache');
+            const faqs = await Faq.find();
 
-        // have to add the redias
+            const translatedFaqs = faqs.map((faq) => faq.getTranslatedText(lang));
 
+            await client.set(cacheKey, JSON.stringify(translatedFaqs), 'EX', 3600);
 
-        const faqs = await Faq.find();
-
-        const translatedFaqs = faqs.map((faq)=> {
-            return faq.getTranslatedText(lang); // Convert plain object to Mongoose instance and then retured
-        });
-
-
-        res.status(200).json({success: true, translatedFaqs});
-
-
-        
+            res.status(200).json({ success: true, translatedFaqs });
+        }
     } catch (error) {
         console.log(error);
-
-        res.status(500).json({success: false, message: "Server Error"});
-        
+        res.status(500).json({ success: false, message: 'Server Error' });
     }
+};
 
-
-}
-
-export const createFaq  = async(req, res)=>{
-
+export const getSingleFaq = async (req, res) => {
     try {
+        const { faqId } = req.params;
+        const lang = req.query.lang || 'en';
+        const cacheKey = `faq_${faqId}_lang_${lang}`;
 
-        const {question, answer, translations} = req.body;
+        const cachedFaq = await client.get(cacheKey);
+        if (cachedFaq) {
+            console.log('Cache hit');
+            return res.status(200).json({
+                success: true,
+                translatedFaq: JSON.parse(cachedFaq),
+            });
+        } else {
+            console.log('Not found in cache');
 
-        console.log(translations);
+            const faq = await Faq.findById(faqId);
 
-        const newFaq = new Faq({question, answer, translations});
+            if (!faq) {
+                return res.status(404).json({ success: false, message: 'FAQ not found' });
+            }
+
+            const translatedFaq = faq.getTranslatedText(lang);
+
+            await client.set(cacheKey, JSON.stringify(translatedFaq), 'EX', 3600);
+
+            res.status(200).json({ success: true, translatedFaq });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Server Error' });
+    }
+};
+
+export const createFaq = async (req, res) => {
+    try {
+        const { question, answer, translations } = req.body;
+
+        const newFaq = new Faq({ question, answer, translations });
 
         await newFaq.save();
 
-        res.status(201).json({success: true, newFaq});
-
-        
+        res.status(201).json({ success: true, newFaq });
     } catch (error) {
-
         console.log(error);
-
-        res.status(500).json({success: false, message: "Server Error"});
-
-        
+        res.status(500).json({ success: false, message: "Server Error" });
     }
-
-}
+};
