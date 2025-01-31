ad616d142903e8405ddde7363eb8bc7783b0ceff
Baljit Singh <thebaljitsinghin@gmail.com>
2025-01-31 22:12:33 +0530

    feat: added faq model for multi lang support

--- model/faqModel.js
+++ model/faqModel.js
@@ -3,9 +3,34 @@ import mongoose from 'mongoose'
 
 const faqSchema = mongoose.Schema({
 
+    question:{
+        type: String,
+        required: true
+    },
+    answer:{
+        type: String,
+        required: true
+    },
+    translations:{
+        question_hi: {
+            type: String
+        },
+        answer_hi:{
+            type: String
+        }
+    }
+
     
 
-})
+});
+
+faqSchema.method.getTranslatedText = (lang)=>{
+    // dynamically fetch the translated faq
+    return {
+        question: this.translations[`question_${lang}`] || this.question,
+        answer: this.translations[`answer_${lang}`] || this.answer
+    }
+}
 
 const Faq = mongoose.model("Faq", faqSchema);
 
