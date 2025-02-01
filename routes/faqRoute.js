a29794537132b042fb07020ab38574190f755995
Baljit Singh <thebaljitsinghin@gmail.com>
2025-02-01 14:47:34 +0530

    feat: added custom route

--- routes/faqRoute.js
+++ routes/faqRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {getAllFaq} from "../controller/faqController.js"
+import {getAllFaq, createFaq, getSingleFaq} from "../controller/faqController.js"
 
 
 const router = express.Router();
 
 router.route("/faqs").get(getAllFaq)
-
+router.route("/faqs").post(createFaq);
+router.route("/faqs/:faqId").get(getSingleFaq);
 
 export default router;
